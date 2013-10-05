60f439bbe85a2f59a6c80956fcac8d6bf0b511ce
Albert Ng <albertng@ctcf.(none)>
2013-10-05 15:11:27 -0700

    Smithwaterman_loop.py traceback

--- scripts/SmithWaterman_loop.py
+++ scripts/SmithWaterman_loop.py
@@ -13,9 +13,12 @@ def SmithWaterman(short_read, reference):
     gap_extend = -1
 
     max_score = 0
+    max_row = 0
+    max_col = 0
     V = [[0] * (len(short_read) + 1) for i in range(len(reference) + 1)]
     E = [[0] * (len(short_read) + 1) for i in range(len(reference) + 1)]
     F = [[0] * (len(short_read) + 1) for i in range(len(reference) + 1)]
+    V_dir = [['Z'] * (len(short_read) + 1) for i in range(len(reference) + 1)]
     for i in range(1, len(V)):
         for j in range(1, len(V[i])):
             E[i][j] = Max([V[i][j-1] + gap_open, E[i][j-1] + gap_extend])
@@ -26,13 +29,28 @@ def SmithWaterman(short_read, reference):
             else:
                 match_score = V[i-1][j-1] + mismatch
             V[i][j] = Max([0, E[i][j], F[i][j], match_score])
+            if (V[i][j] == 0):
+                V_dir[i][j] = 'Z'
+            elif (V[i][j] == E[i][j]):
+                V_dir[i][j] = 'I'
+            elif (V[i][j] == F[i][j]): 
+                V_dir[i][j] = 'D'
+            else:
+                V_dir[i][j] = 'M'
             if V[i][j] > max_score:
                 max_score = V[i][j]
-     
+                max_row = i
+                max_col = j
     print 'V:'
     for row in range(len(V)):
         for col in range(len(V[0])):
             print(str(V[row][col]) + '\t'),
+        print ''
+    print ''
+    print 'V_dir:'
+    for row in range(len(V_dir)):
+        for col in range(len(V[0])):
+            print(str(V_dir[row][col]) + '\t'),
         print ''
     print ''
     print 'E:'
@@ -47,7 +65,35 @@ def SmithWaterman(short_read, reference):
             print(str(F[row][col]) + '\t'),
         print ''
     print ''
-    return max_score
+    return (max_score, max_row, max_col, V_dir)
 
-#print(SmithWaterman("ACAGACTA", "AGCACACA"))
-print(SmithWaterman("ACAGACTA", "ACACTA"))
+ref = "AGAAAGTCA"
+query = "GGTCAG"
+(max_score, max_row, max_col, V_dir) = SmithWaterman(ref, query)
+print(max_score)
+
+# Perform Traceback
+aln1 = ""
+aln2 = ""
+row = max_row
+col = max_col
+decisions = ""
+while (V_dir[row][col] != 'Z'):
+    print(row, col)
+    decisions = V_dir[row][col] + decisions
+    if (V_dir[row][col] == 'M'):
+        aln1 = ref[col-1] + aln1
+        aln2 = query[row-1] + aln2
+        row -= 1
+        col -= 1
+    elif (V_dir[row][col] == 'I'):
+        aln1 = ref[col-1] + aln1
+        aln2 = "-" + aln2
+        col -= 1
+    elif (V_dir[row][col] == 'D'):
+        aln1 = "-" + aln1
+        aln2 = query[row-1] + aln2
+        row -= 1
+print(aln1)
+print(aln2)
+print(decisions)
