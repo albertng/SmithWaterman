2b91454a8629132510d61795bf283761c5fd3046
Albert Ng <albertng@ctcf.(none)>
2014-02-13 12:37:20 -0800

    Removed large unused directories

---
+++ SmithWatermanAccelerator/software/src/dispatchworkerthread.cpp
@@ -0,0 +1,95 @@
+//  File Name        : dispatchworkerthread.cpp
+//  Description      : FPGA Engine Alignment Dispatch Worker Thread definitions
+//
+//  Revision History :
+//      Albert Ng   Feb 11 2014     Initial Revision
+
+#include <pthread.h>
+#include <assert.h>
+#include "dispatchworkerthread.h"
+#include "def.h"
+#include "threadqueue.h"
+#include "utils.h"
+#ifdef SIM_PICO
+  #include "picodrv_sim.h"
+#else
+  #include <picodrv.h>
+  #include <pico_errors.h>
+#endif
+
+DispatchWorkerThread::DispatchWorkerThread() {
+}
+
+DispatchWorkerThread::DispatchWorkerThread(PicoDrv* pico_driver, int stream, 
+                                           ThreadQueue<DispatchJob>* dispatch_job_queue) {
+  Init(pico_driver, stream, dispatch_job_queue);
+}
+
+void DispatchWorkerThread::Init(PicoDrv* pico_driver, int stream, 
+                                ThreadQueue<DispatchJob>* dispatch_job_queue) {
+  args_.pico_driver = pico_driver;
+  args_.stream = stream;
+  args_.dispatch_job_queue = dispatch_job_queue;
+}
+
+void DispatchWorkerThread::Run() {
+  pthread_create(&thread_, NULL, &DispatchWorkerThread::Dispatch, (void*) (&args_));
+}
+
+void DispatchWorkerThread::Join() {
+  pthread_join(thread_, NULL);
+}
+
+void* DispatchWorkerThread::Dispatch(void* args) {
+  // Get thread arguments
+  PicoDrv* pico_driver = ((DispatchWorkerThreadArgs*)args)->pico_driver;
+  int stream = ((DispatchWorkerThreadArgs*)args)->stream;
+  ThreadQueue<DispatchJob>* dispatch_job_queue = ((DispatchWorkerThreadArgs*)args)->dispatch_job_queue;
+  
+  uint32_t* dispatch_buf = new uint32_t[DISPATCH_BUF_LEN];
+  int cur_buf_size = 0;
+  while(true) {
+    DispatchJob job = dispatch_job_queue->Pop();
+    
+    // Compute buffer space needed for this job
+    int job_buf_size = 4 * (1 + job.query_len / QUERY_BLOCK_LEN);
+    if (job.query_len % QUERY_BLOCK_LEN != 0) {
+      job_buf_size += 4;
+    }
+    
+    // Dispatch and empty the buffer before adding this job
+    //   if it will overflow
+    if (cur_buf_size + job_buf_size > DISPATCH_BUF_LEN) {
+      assert(cur_buf_size > 0);
+      pico_driver->WriteStream(stream, dispatch_buf, cur_buf_size * 4);
+      cur_buf_size = 0;
+    }
+    
+    // Add job to the buffer
+    dispatch_buf[cur_buf_size + 0] = (uint32_t) job.num_ref_blocks;
+    dispatch_buf[cur_buf_size + 1] = (uint32_t) job.first_ref_block;
+    dispatch_buf[cur_buf_size + 2] = (uint32_t) ((job.query_id << 16) + job.query_len);
+    dispatch_buf[cur_buf_size + 3] = (uint32_t) job.threshold;
+    for (int i = 0; i < job.query_len; i++) {
+      NtInt nt = NtChar2Int(job.query_seq[i]);
+      if (job.query_seq[i] == 'N' || job.query_seq[i] == 'n') {
+        nt = rand() % 4;
+      }
+      if (i % 16 == 0) {
+        dispatch_buf[cur_buf_size + 4+i/16] = nt;
+      } else {
+        dispatch_buf[cur_buf_size + 4+i/16] += (nt << (2 * (i % 16)));
+      }
+    }
+    cur_buf_size += job_buf_size;
+    
+    // Dispatch and empty the buffer if the job queue is now empty
+    if (dispatch_job_queue->Empty()) {
+      assert(cur_buf_size > 0);
+      pico_driver->WriteStream(stream, dispatch_buf, cur_buf_size * 4);
+      cur_buf_size = 0;
+    }
+  }
+}
+
+
---
+++ SmithWatermanAccelerator/software/src/dispatchworkerthread.h
@@ -0,0 +1,69 @@
+//  File Name        : dispatchworkerthread.h
+//  Description      : FPGA Engine Alignment Dispatch Worker Thread definitions
+//
+//  Revision History :
+//      Albert Ng   Feb 11 2014     Initial Revision
+
+#ifndef DISPATCHWORKERTHREAD_H_
+#define DISPATCHWORKERTHREAD_H_
+
+#include <pthread.h>
+#include "def.h"
+#include "threadqueue.h"
+#ifdef SIM_PICO
+  #include "picodrv_sim.h"
+#else
+  #include <picodrv.h>
+  #include <pico_errors.h>
+#endif
+
+class DispatchWorkerThread {
+  public:
+    // Empty constructor
+    DispatchWorkerThread();
+    
+    // Complete constructor
+    DispatchWorkerThread(PicoDrv* pico_driver, int stream, 
+                         ThreadQueue<DispatchJob>* dispatch_job_queue);
+                         
+    // Initialization function (called by constructor)
+    void Init(PicoDrv* pico_driver, int stream, 
+              ThreadQueue<DispatchJob>* dispatch_job_queue);  
+
+    // Run the thread
+    void Run();
+
+    // Join the thread
+    //   Can't be used for now, since the thread infinite loops and never terminates
+    void Join();
+  
+  private:
+    // FPGA engine dispatch thread arguments struct
+    struct DispatchWorkerThreadArgs {
+      // Pointer to FPGA driver
+      PicoDrv* pico_driver;
+      
+      // Engine stream handle
+      int stream;
+      
+      // Pointer to shared queue of jobs to be dispatched to this engine
+      ThreadQueue<DispatchJob>* dispatch_job_queue;
+    };
+    
+    // Function for thread to run.
+    //   Continuously retrieves alignment jobs from alignment job queue, splits the alignment
+    //   jobs into engine sub-jobs, sends the engine sub-jobs to the FPGA engines, and
+    //   records the scheduled engine jobs onto the engine job queues.
+    static void* Dispatch(void* args);
+    
+    // Actual pthread instance
+    pthread_t thread_;
+
+    // Thread arguments
+    DispatchWorkerThreadArgs args_;
+    
+    // Buffer length (in 4B quantities)
+    static const int DISPATCH_BUF_LEN = 4*1024*1024; // 16 MB 
+};
+
+#endif // DISPATCHWORKERTHREAD_H_
