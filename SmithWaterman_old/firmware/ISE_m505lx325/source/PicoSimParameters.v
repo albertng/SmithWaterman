ea86ccb149d214ba721f1e6040a4c3338b976d5b
Albert Ng <albertng@ctcf.(none)>
2014-02-12 20:14:27 -0800

    Removed SmithWaterman_old

--- SmithWaterman_old/firmware/ISE_m505lx325/source/PicoSimParameters.v
+++
@@ -1,7 +0,0 @@
-`include "PicoDefines.v"
-`define data_buf_size 4096 // must be a multiple of 128 bits in byte
-
-//`define FULL_DEBUG // turns on a bunch of debug messages, only use if you need it
-
-`define PICO_DDR3_0 0
-`define PICO_DDR3_1 1
